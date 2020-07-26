28377e0a8adccc8ed0b54f2d06859e104833217a
Joseph Wang <jamesxxxxxxx@gmail.com>
2020-07-26 23:12:22 +0800

    update

--- script/build.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs-extra');
-
-const allTransitions = [
-  { name: 'Fade', enter: { opacity: 1 }, leave: { opacity: 0 } }
-];
---
+++ scripts/test2.js
@@ -0,0 +1,10 @@
+// const fs = require('fs-extra');
+// const fs = require('fs');
+// const path = require('path');
+// const appDirectory = fs.realpathSync(process.cwd());
+
+console.log('appDirectory');
+
+// const allTransitions = [
+//   { name: 'Fade', enter: { opacity: 1 }, leave: { opacity: 0 } }
+// ];
